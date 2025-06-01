723567896c47de4d056880da0319dd766d3c8d55
John <jonatascleytonf@gmail.com>
2025-05-31 21:46:22 -0300

    Questao 3

--- ex8/q3/Program.java
+++ ex8/q3/Program.java
@@ -1,5 +1,15 @@
 class Program{
     public static void main(String[] args) {
+        Classe cls = new Classe();
+        System.out.println("Param antes era: " + cls.param);
+        alteraParamDeEntrada(cls);
+        System.out.println("agora Param é: " + cls.param);
+
+    }
+
+    public static void alteraParamDeEntrada(Classe entrada){
+        System.out.println("alterado dentro da função para 1");
+        entrada.param = 1;
 
     }
 }
